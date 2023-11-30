ed7245a5be3358ae500652e0d21f2e715cb654ab
横山大希 <70083780+Daikisan212@users.noreply.github.com>
2023-11-30 19:54:24 +0900

    説明まで完了

--- portfolio1/src/App.tsx
+++ portfolio1/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import NavBar from './components/NavBar';
 import MyAvatar from './components/MyAvatar';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <NavBar />
       <MyAvatar />
+      <About />
     </>
   );
 }
